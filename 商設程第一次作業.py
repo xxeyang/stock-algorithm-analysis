d847eddedb09565f1378c569343e0db195f5b195
ken chen <ken0965106303@gmail.com>
2019-12-06 12:32:22 +0800

    Create 商設程第一次作業.py

---
+++ 商設程第一次作業.py
@@ -0,0 +1,10 @@
+while(1):
+	a = int(input())
+	change = 100 - a
+	ans = [] 
+	coin = [50,10,5,1]
+	for i in range(0,len(coin)):
+		ans.append(change // coin[i])
+		change -= ans[i] * coin[i]
+	for j in ans:
+		print(j)
